321ccbe3fa3c80441660361c77450955256ca736
roock89 <77836346+HenrysHouses@users.noreply.github.com>
2022-11-11 23:50:13 +0100

    Licence updates

--- Runtime/SceneCollectionObject.cs
+++ Runtime/SceneCollectionObject.cs
@@ -1,3 +1,21 @@
+// * Multi Scene Management Tools For Unity
+// *
+// * Copyright (C) 2022  Henrik Hustoft
+// *
+// * This program is free software: you can redistribute it and/or modify
+// * it under the terms of the GNU General Public License as published by
+// * the Free Software Foundation, either version 3 of the License, or
+// * (at your option) any later version.
+// *
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// * GNU General Public License for more details.
+// *
+// * You should have received a copy of the GNU General Public License
+// * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +24,6 @@ using UnityEditor;
 
 namespace HH.MultiSceneTools
 {
-    // TODO add scene collection hiarchy preview???
-    // take features from https://assetstore.unity.com/packages/tools/utilities/multiscene-24921 and https://assetstore.unity.com/packages/tools/utilities/advanced-multi-scene-cross-scene-references-47200#reviews
-    // display if scene is in build
-    // cross scene references
-
     [CreateAssetMenu(menuName = "Multi Scenes/SceneCollectionObject")]
     public class SceneCollectionObject : ScriptableObject
     {
