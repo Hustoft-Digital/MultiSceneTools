ee96204cb7d3c8052b593e5c90f4f83bca34d7af
roock89 <77836346+HenrysHouses@users.noreply.github.com>
2022-11-13 19:19:30 +0100

    Fix

--- Runtime/MultiSceneLoader.cs
+++ Runtime/MultiSceneLoader.cs
@@ -16,8 +16,6 @@
 // * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Events;
