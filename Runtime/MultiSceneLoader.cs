b00c22c9a7aacad0ba2b05f9d8356afd987764f1
Henry <77836346+HenrysHouses@users.noreply.github.com>
2023-10-16 10:16:32 +0200

    Update MultiSceneLoader.cs

--- Runtime/MultiSceneLoader.cs
+++ Runtime/MultiSceneLoader.cs
@@ -13,7 +13,7 @@
 // *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // *   See the License for the specific language governing permissions and
 // *   limitations under the License.
-
+#define MULTI_SCENES_ASYNC
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Events;
