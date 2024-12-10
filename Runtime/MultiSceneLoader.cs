e97126e2513c4e19474466e4d1b971e9cc40e667
Henry <77836346+HenrysHouses@users.noreply.github.com>
2024-12-10 04:02:36 +0100

    Update MultiSceneLoader.cs

--- Runtime/MultiSceneLoader.cs
+++ Runtime/MultiSceneLoader.cs
@@ -80,7 +80,7 @@ namespace HH.MultiSceneTools
             if(asyncLoadingTask.Count == 0)
             {
                 collectionsCurrentlyLoaded.Clear();
-
+                Debug.Log("Clearing open scene collections");
                 #if UNITY_EDITOR
                     MultiSceneToolsConfig.instance.SetCurrentCollectionEmpty(); 
                 #endif
